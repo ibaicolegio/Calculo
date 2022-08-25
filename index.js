662b9ae9b756f022e17c4f97f1631e875298276b
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2022-08-25 22:38:47 +0200

    25/08/2022

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let total=0;
+
 function material(){
     let material=document.getElementById("Material").value;
     const materialArray = material.split(" ");
@@ -16,7 +18,40 @@ function calcular(){
     let precio=document.getElementById("precio").value;
     if(largo!="" & ancho!="" & espesor!="" & densidad!="" & merma!="" & precio!=""){
         document.getElementById("total").style.display = "block";
-        let total=largo*ancho*espesor*densidad*merma*precio;
+        total=Number.parseFloat(largo*ancho*espesor*densidad*merma*precio).toFixed(2);
         document.getElementById("cantTotal").value = total;
     }
+}
+
+function lista(){
+    if(localStorage.materiales==undefined){
+        localStorage.materiales="";
+    } else if(localStorage.materiales!=""){
+        let materiales=localStorage.materiales;
+        const materialesArray = materiales.split(";");
+        let elementos="";
+        for (const iterator of materialesArray) {
+            elementos+=`<li class="list-group-item">`+iterator+`</li>`;
+        }
+        document.getElementById("elementosLista").innerHTML = elementos;
+        document.getElementById("lista").style.display = "block";
+    } else {
+        let elementos="";
+        document.getElementById("elementosLista").innerHTML = elementos;
+        document.getElementById("lista").style.display = "none";
+    }
+}
+
+function anyadir(){
+    let total=document.getElementById("cantTotal").value;
+    let materiales=localStorage.materiales;
+    const materialesArray = materiales.split(";");
+    let tamanyo=materialesArray.length;
+    localStorage.materiales += ["Material "+tamanyo+":"+total+";"];
+    lista();
+}
+
+function borrar(){
+    localStorage.materiales="";
+    lista();
 }
